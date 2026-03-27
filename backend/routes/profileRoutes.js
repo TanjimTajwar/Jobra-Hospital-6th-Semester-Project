f1d020ca29a1363807bc5e4b5455832ac7eb5e0b
tanjimtajwar <tanjimtajwar@yahoo.com>
2026-03-28 00:28:05 +0600

    last add

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -6,5 +6,6 @@ const c = require('../controllers/profileController');
 const auth = require('../middleware/auth');
 
 router.get('/me', auth, c.myProfile);
+router.put('/update', auth, c.updateProfile);
 
 module.exports = router;
