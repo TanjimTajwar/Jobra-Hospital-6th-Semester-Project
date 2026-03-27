a5e1f029f87bcd6a2c8debb3b92db7d92a7998e4
tanjimtajwar <tanjimtajwar@yahoo.com>
2026-03-28 00:48:37 +0600

    src added

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: "http://localhost:5000/api"
+});
+
+// attach token automatically
+API.interceptors.request.use(req => {
+
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user?.token) {
+        req.headers.Authorization = user.token;
+    }
+
+    return req;
+});
+
+export default API;
