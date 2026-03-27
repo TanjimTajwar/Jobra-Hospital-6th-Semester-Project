24f539b7ea0cd676821d85ca9da5654409c075f0
tanjimtajwar <tanjimtajwar@yahoo.com>
2026-03-28 00:35:20 +0600

    all deleted

--- backend/models/doctorModel.js
+++
@@ -1,6 +0,0 @@
-const db = require('../config/db');
-
-exports.getAllDoctors = (callback) => {
-    const sql = "SELECT doctor_id, doctor_name, department, qualification, experience, chamber_time FROM jh_doctors";
-    db.query(sql, callback);
-};
